1c3490946a829f6f6d1bfe36214d6f771bce67e5
Boonchai Wedwanitsatheiin <neng87@gmail.com>
2024-07-04 14:13:43 +0700

    update code

--- src/main/java/com/nengboonchai/demo/controller/HomeController.java
+++ src/main/java/com/nengboonchai/demo/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.nengboonchai.demo.controller;
 import com.nengboonchai.demo.kafka.ProducerKafaService;
 import com.nengboonchai.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("appName",appName);
         return "home";
     }
     @GetMapping("/hello")
